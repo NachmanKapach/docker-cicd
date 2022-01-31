92f7e74f83b59bdc8d7bbe9fdc03d91af4406853
NachmanKapach <93979588+NachmanKapach@users.noreply.github.com>
2022-01-31 20:47:12 +0200

    Update nodejs_docker.groovy

--- basics/jenkins/job-dsl/nodejs_docker.groovy
+++ basics/jenkins/job-dsl/nodejs_docker.groovy
@@ -15,7 +15,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('nachman/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('dockernachman')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
